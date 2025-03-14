10bb9e2381f2aa367aa0531161ca3f274b1a89f3
Januario <januario86fp@gmail.com>
2025-03-13 23:14:11 -0300

    feat:inclusão do pacote security - Contendo a Classe de configuração inicial para incrementar a camada de segurança

---
+++ src/main/java/com/pet/api/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.pet.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/**").permitAll() // Endpoints públicos
+                .anyRequest().authenticated() // Todos os outros endpoints exigem autenticação
+            )
+            .httpBasic(); // Usar Basic Authentication
+        return http.build();
+    }
+}
